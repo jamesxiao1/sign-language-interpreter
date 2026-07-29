8328985bf2f1b0e4b639a2a6341752a657c568f5
jamesxiao1 <jamesxiao039@gmail.com>
2026-07-28 20:57:25 -0400

    model training and classifider for a,b,l

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import tensorflow as tf
 import sys 
 import csv 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
 
 def normalize_hand(row):
     # a row is 63 nums for one hand
@@ -44,7 +46,47 @@ x=np.array(normalized)
 # print("x shape",x.shape)
 # print("first piont of row 0",x[0][:3])
 
+x_train,x_test,y_train,y_test = train_test_split(
+    x,y,
+    test_size=0.2, # hold 20% for testing,80% for training 
+    stratify=y, # piles keep the same proportion of each letter 
+    random_state=42 # random number, for reproducible results
+)
 
+# print("train:", x_train.shape, y_train.shape)
+# print("test:", x_test.shape, y_test.shape)
+# print("train labels:", np.bincount(y_train)) 
+# print("test labels:", np.bincount(y_test))
 
+model=tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(63,)), # define shape of input data,each training example has 63 features
+    tf.keras.layers.Dense(64,activation="relu"), # dense is fully connected layer, 64 is 64 neurons, each neuron recieves info from all 63 features
+    tf.keras.layers.Dense(32,activation="relu"), # 32 neurons, 64 inputs each 
+    tf.keras.layers.Dense(3,activation="softmax") # 3 neurons for a,b,l
+
+    # input width =feature count(63), output width=class count(3)
+])
+
+model.compile( # configures settings before training?
+    optimizer="adam", # smart nudger, gradient descent
+    loss="sparse_categorical_crossentropy",#wrongness score
+    metrics=["accuracy"]
+)
+
+model.fit( #guess,measure,blame,nudge loop
+    x_train,y_train, 
+    validation_data=(x_test,y_test),
+    epochs=50,# loops 
+    batch_size=16 #process 16 examples ata time before each nudge, faster than one at a time 
+)
+
+"""
+loss -wrongness in data,show drop close to 0 as epoch inc 
+accuracy-% correct in data, should rise toward 1.0
+val_loss/val_accuracy-same as before but for held-back test set, val accuracy is important, should rise to 1.0
+"""
 
 data_file.close()
+model.save("sign_model.keras")
+np.save("label_classes.npy",encoder.classes_)
+print("saved model and labels")
